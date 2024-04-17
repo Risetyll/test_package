871430a12441c36e789a2d59f3911fb00c852891
Risetyll <bidros2002@mail.ru>
2024-04-17 20:34:28 +0700

    create test package

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/risetyll/test_package
+
+go 1.22.1
---
+++ test_package.go
@@ -0,0 +1,7 @@
+package test_package
+
+import "fmt"
+
+func Hi(name string) string {
+	return fmt.Sprintf("Hello, %s", name)
+}
